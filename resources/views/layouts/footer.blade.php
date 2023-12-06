3de10788382088f23faca7caa6a6e04106bac868
Himanshu Singhal <himanshu.singhal@Karam.in>
2023-12-06 15:45:09 +0530

    chnage static url

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -43,7 +43,7 @@
 <script>
    
    /***************get email address************************/
-    var urls= 'http://127.0.0.1:8000/';
+    var urls= 'http://localhost:8000/';
     $("#email").blur(function (){
        var email = $(this).val();
        var re = /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/igm;
