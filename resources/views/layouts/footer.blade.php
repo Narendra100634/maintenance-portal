c65d0850967b08b5a15417cae8421a38143ad1e6
Himanshu Singhal <himanshu.singhal@karam.in>
2023-12-25 09:42:23 +0530

    chnages update dashboard

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,4 +1,4 @@
 <footer class="main-footer">
-    <div class="pull-right hidden-xs"><b>Version</b> 1.2.0</div>
+    <div class="pull-right hidden-xs"><b>Version</b> 1.0.0</div>
     <span>© Copyrights KARAM Safety Pvt. Ltd. 2023. All rights reserved.</span>
 </footer>
